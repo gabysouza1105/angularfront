20c51eb61af978effa0a0514a14e07160abeeb0d
Gabriela Souza <gaby.souza1105@gmail.com>
2021-12-05 13:15:48 -0300

    .

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'frontend';
+}
